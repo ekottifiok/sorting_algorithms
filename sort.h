5554f59a2f98bb75375b2dcc4fed518214d80efd
jude iwuji <judeiwuji@gmail.com>
2022-10-06 11:46:31 +0100

    Update: Added selecton sort

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,5 +15,6 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-
+void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
