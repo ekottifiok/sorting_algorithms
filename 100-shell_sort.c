1467ace1c61c08af82a56ec5dbc6b19363804524
Ifiok Ekott <ekottifiok@gmail.com>
2022-10-07 16:44:12 +0100

    added _pow_recursion and generate_knuth

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,3 +1,19 @@
+#include "sort.h"
+
+/**
+ * _pow_recursion - finds the power of number using recursion
+ * @x: the base value example 10
+ * @y: the power example 2
+ * Return: returns the value of 10**2 which is 100
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
 
 /**
  * print_db_linklist - handles the printing of the double list
@@ -18,7 +34,25 @@ void print_db_linklist(listint_t *buffer)
 	printf("\n");
 }
 
-void insertion_sort_list(listint_t **list)
+int *generate_knuth(int end)
+{
+    int *ar;
+    unsigned int i, step, len_ar = 1;
+
+    ar = malloc(sizeof(int)*len_ar);
+    ar[len_ar-1] = 0;
+    for (i=1; i<end; i++)
+    {
+        step = (_pow_recursion(3, i) - 1)/2;
+        if (step > end)
+            break;
+        ar[len_ar-1] = step;
+        ar[len_ar] = 0;
+        ar = realloc(ar, ++len_ar);
+    }
+}
+
+void shell_sort(int *array, size_t size)
 {
 
 }
