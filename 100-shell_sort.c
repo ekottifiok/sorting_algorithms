7599a398cf062020dbc3ee141c5d84d18f7b370a
Ifiok Ekott <ekottifiok@gmail.com>
2022-10-07 16:56:44 +0100

    fixed shell sort and sort.h

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -15,44 +15,28 @@ int _pow_recursion(int x, int y)
 	return (x * _pow_recursion(x, y - 1));
 }
 
-/**
- * print_db_linklist - handles the printing of the double list
- *
- * @buffer: list to be printed
- */
-void print_db_linklist(listint_t *buffer)
+size_t generate_knuth(int **a, int end)
 {
-	while (buffer->prev)
-		buffer = buffer->prev;
-	for (; buffer; buffer = buffer->next)
-	{
-		if (buffer->prev)
-			printf(", %d", buffer->n);
-		else
-			printf("%d", buffer->n);
-	}
-	printf("\n");
-}
-
-int *generate_knuth(int end)
-{
-    int *ar;
-    unsigned int i, step, len_ar = 1;
+    int *ar = *a;
+    unsigned int i, step;
+    size_t len_ar = 1;
 
     ar = malloc(sizeof(int)*len_ar);
-    ar[len_ar-1] = 0;
-    for (i=1; i<end; i++)
+    ar[0] = 0;
+    for (i=1; i<end; i++, len_ar++)
     {
         step = (_pow_recursion(3, i) - 1)/2;
         if (step > end)
             break;
         ar[len_ar-1] = step;
+        ar = realloc(ar, len_ar+1);
         ar[len_ar] = 0;
-        ar = realloc(ar, ++len_ar);
     }
+    return len_ar;
 }
 
 void shell_sort(int *array, size_t size)
 {
-
+    int *ar;
+    print_array(ar, generate_knuth(&ar, 6));
 }
--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 #endif
