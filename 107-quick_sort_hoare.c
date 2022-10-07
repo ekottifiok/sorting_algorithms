4f114694b62570e94ffc1e8173b800ca6f36c269
jude iwuji <judeiwuji@gmail.com>
2022-10-07 14:10:11 +0100

    New: Quick-Sort-Hoare

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - Sorts an array using the hoare
+ * partition algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The array size
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+}
--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,14 +11,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 unsigned int check_for_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void quick_sort_hoare(int *array, size_t size);
 #endif
