c0bbc53e82d178a2b5f8abb22c9706d396228140
Ifiok Ekott <ekottifiok@gmail.com>
2022-10-09 13:42:02 +0100

    Started working on merge sort almost done trying to fix the print part

--- sort.h
+++ sort.h
@@ -33,4 +33,5 @@ void shell_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 #endif
