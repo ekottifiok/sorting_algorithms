cfe99eb8c7d4adad23d535c707a298e9ebe25192
Ifiok Ekott <ekottifiok@gmail.com>
2022-10-11 08:42:56 +0100

    fixed betty errors all good to go runs well with all error flags but the result has a minor error

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,18 +1,16 @@
 #include "deck.h"
 
-int present_position(deck_node_t *start)
-{
-    int len_list;
-    for (len_list = 0; start; len_list++)
-		start = start->next;
-    return len_list;
-}
-
-void print_deck_sd(deck_node_t *deck)
+/**
+ * @brief meant for trouble shooting
+ * 
+ * @param deck 
+ */
+void print_dec(const deck_node_t *deck)
 {
     size_t i;
     char kinds[4] = {'S', 'H', 'C', 'D'};
-    while (deck->prev)
+
+	while (deck->prev)
 		deck = deck->prev;
     i = 0;
     while (deck)
@@ -27,68 +25,96 @@ void print_deck_sd(deck_node_t *deck)
     }
 }
 
-void move_forward(deck_node_t **main, int *flag, char *keys, int position_end)
+/**
+ * present_position - computes the present position for deck_node_t
+ *
+ * @start: where the pointer starts
+ * Return: int
+ */
+int present_position(deck_node_t *start)
 {
-    deck_node_t *buffer = *main, *node, *prev_node, *next_node;
-    int key_position, kind_value;
+	int len_list;
 
-    kind_value = position_end %10;
-    position_end /= 10;
-    key_position = position_end % 10;
-    position_end /= 10;
-    key_position += (position_end % 10) * 10;
-    for (position_end /= 10; buffer->next && position_end > 0; position_end--)
-    {
-        prev_node = buffer->prev;
-        next_node = buffer->next;
-        node = buffer;
-        if ((int)(node->card->kind) == kind_value && *(node->card->value) == keys[key_position])
-        {
-
-            if (prev_node)
-                prev_node->next = next_node;
-            next_node->prev = prev_node;
-            if (next_node->next != NULL)
-                (next_node->next)->prev = node;
-            node->next = next_node->next;
-            next_node->next = node;
-            node->prev = next_node;
-
-            *flag = 1;
-            print_deck_sd(*main);
-            continue;
-        }
-
-        buffer = buffer->next;
-    }
-    *main = buffer;
+	for (len_list = 0; start; len_list++)
+		start = start->next;
+	return (len_list);
 }
 
-void move_backward(deck_node_t **main, int *flag, char *keys, int position_end)
+/**
+ * move_forward - moves the specified card forward
+ *
+ * @main: list to be worked on
+ * @keys: the keys of the card
+ * @position_end: data as to position and end of variables
+ */
+void move_forward(deck_node_t **main, char *keys, int position_end)
 {
-    deck_node_t *buffer = *main, *node, *prev_node, *next_node;
-    int key_position, kind_value, position_sub_flag = 1;
-    char *inverted_key = "3210";
+	deck_node_t *buffer = *main, *node, *prev_node, *next_node;
+	int key_position, kind_value;
 
-    kind_value = inverted_key[position_end%10]-48;
-    position_end /= 10;
-    key_position = position_end % 10;
-    position_end /= 10;
-    key_position += (position_end % 10) * 10;
-    position_end /= 10;
-    for (; buffer->prev; )
-    {
-        prev_node = buffer->prev;
-        next_node = buffer->next;
-        node = buffer;
-        if ((int)(node->card->kind) == kind_value && *(node->card->value) == keys[key_position] && position_end >= 0)
-        {
-            if (position_sub_flag)
-            {
-                position_end -= present_position(buffer);
-                position_sub_flag = 0;
-            }
-            if (prev_node)
+	kind_value = position_end % 10;
+	position_end /= 10;
+	key_position = position_end % 10;
+	position_end /= 10;
+	key_position += (position_end % 10) * 10;
+	for (position_end /= 10; buffer->next && position_end > 0; position_end--)
+	{
+		prev_node = buffer->prev;
+		next_node = buffer->next;
+		node = buffer;
+		if ((int)(node->card->kind) == kind_value &&
+			*(node->card->value) == keys[key_position])
+		{
+
+			if (prev_node)
+				prev_node->next = next_node;
+			next_node->prev = prev_node;
+			if (next_node->next != NULL)
+				(next_node->next)->prev = node;
+			node->next = next_node->next;
+			next_node->next = node;
+			node->prev = next_node;
+
+			continue;
+		}
+
+		buffer = buffer->next;
+	}
+	*main = buffer;
+}
+
+/**
+ * move_backward - moves the specified card backward
+ *
+ * @main: list to be worked on
+ * @keys: the keys of the card
+ * @position_end: data as to position and end of variables
+ */
+void move_backward(deck_node_t **main, char *keys, int position_end)
+{
+	deck_node_t *buffer = *main, *node, *prev_node, *next_node;
+	int key_position, kind_value, position_sub_flag = 1;
+	char *inverted_key = "3210";
+
+	kind_value = inverted_key[position_end % 10] - 48;
+	position_end /= 10;
+	key_position = position_end % 10;
+	key_position += ((position_end / 10) % 10) * 10;
+	position_end /= 100;
+	for (; buffer->prev;)
+	{
+		prev_node = buffer->prev;
+		next_node = buffer->next;
+		node = buffer;
+		if ((int)(node->card->kind) == kind_value &&
+			*(node->card->value) == keys[key_position] && position_end >= 0)
+		{
+			if (position_sub_flag)
+			{
+				position_end -= present_position(buffer);
+				position_sub_flag = 0;
+			}
+			if (prev_node)
 				prev_node->next = next_node;
 			next_node->prev = prev_node;
 			node->next = prev_node;
@@ -99,49 +125,53 @@ void move_backward(deck_node_t **main, int *flag, char *keys, int position_end)
 					prev_node->prev->next = node;
 				prev_node->prev = node;
 			}
-			*flag = 1;
-            print_deck_sd(*main);
-            position_end--;
-            continue;
-        }
-
-        buffer = buffer->prev;
-    }
-    *main = buffer;
+			position_end--;
+			continue;
+		}
+		buffer = buffer->prev;
+	}
+	*main = buffer;
 }
 
-
-
+/**
+ * sort_deck - entrypoint main function that handles the sorting of the deck
+ *
+ * @deck: deck to be sorted
+ */
 void sort_deck(deck_node_t **deck)
 {
-    int sort_flag, kind_value, key_value, info;
-    char keys[] = "A234567891JQK", keys_rev[] = "KQJ198765432A";
-    deck_node_t *working_deck = *deck;
-    size_t len_list, len_list_buf;
+	int kind_value, key_value, info;
+	char keys[] = "A234567891JQK", keys_rev[] = "KQJ198765432A";
+	deck_node_t *working_deck = *deck;
+	size_t len_list, len_list_buf;
 
-    if (!deck || !(*deck))
-        return;
+	if (!deck || !(*deck))
+		return;
 
-    for (working_deck = *deck;working_deck->prev; working_deck = working_deck->prev)
+	for (working_deck = *deck; working_deck->prev;
+		 working_deck = working_deck->prev)
 		;
-    len_list = present_position(working_deck);
+	len_list = present_position(working_deck);
 
-    for (sort_flag = 1, kind_value = 3, key_value = 0, len_list_buf = --len_list; kind_value > 1; working_deck = *deck, key_value++, len_list_buf--)
-    {
-        if (key_value == 13)
-        {
-            key_value = 0;
-            kind_value--;
-        }
-        while (working_deck->prev)
-		    working_deck = working_deck->prev;
-        sort_flag = 0;
-        info = len_list_buf*1000 + key_value * 10+kind_value;
-        move_forward(&working_deck, &sort_flag, keys_rev, info);
-        working_deck = working_deck->prev;
-        move_backward(&working_deck, &sort_flag, keys, info);
-    }
-    while (working_deck->prev)
-        working_deck = working_deck->prev;
-    *deck = working_deck;
+	for (kind_value = 3, key_value = 0, len_list_buf = --len_list;
+		 kind_value > 1; working_deck = *deck, key_value++, len_list_buf--)
+	{
+		if (key_value == 13)
+		{
+			key_value = 0;
+			kind_value--;
+		}
+		while (working_deck->prev)
+			working_deck = working_deck->prev;
+		info = len_list_buf * 1000 + key_value * 10 + kind_value;
+		move_forward(&working_deck, keys_rev, info);
+		working_deck = working_deck->prev;
+		print_dec(working_deck);
+		move_backward(&working_deck, keys, info);
+		print_dec(working_deck);
+		
+	}
+	while (working_deck->prev)
+		working_deck = working_deck->prev;
+	*deck = working_deck;
 }
